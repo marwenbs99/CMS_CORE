8904fa6f02626dc8e9e2e71186ec151b9638eca8
Marwen Ben Salem <bensalemarwen@gmail.com>
2025-11-27 18:08:10 +0100

    Checkpoint

---
+++ src/OrchardCore.Themes/TheCreativeTheme/Manifest.cs
@@ -0,0 +1,11 @@
+using OrchardCore.DisplayManagement.Manifest;
+using OrchardCore.Modules.Manifest;
+
+[assembly: Theme(
+    Name = "The Creative Theme",
+    Author = ManifestConstants.OrchardCoreTeam,
+    Website = ManifestConstants.OrchardCoreWebsite,
+    Version = ManifestConstants.OrchardCoreVersion,
+    Description = "An attractive one page Bootstrap theme perfect for creative portfolios and businesses.",
+    Tags = ["Bootstrap", "Landing page", "Liquid"]
+)]
